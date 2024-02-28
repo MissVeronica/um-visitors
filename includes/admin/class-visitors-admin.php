5f70ab654c06cefbe1d89e8f35468c5fb0d74e27
MissVeronica <veronica@tranza.se>
2024-02-28 08:28:35 +0100

    Update class-visitors-admin.php

--- includes/admin/class-visitors-admin.php
+++ includes/admin/class-visitors-admin.php
@@ -6,11 +6,12 @@ class Visitors_Admin {
 
     public $heading_text     = '';
     public $directory_forms  = array( 0 => '' );
+    public $new_vv_columns   = array();
+    public $vv_columns       = array();
 
     function __construct() {
 
         add_filter( 'um_settings_structure', array( $this, 'visitors_settings' ), 99, 1 );
-        add_shortcode( 'vv_activity',        array( $this, 'vv_activity_shortcode' ));
 
         if ( UM()->options()->get( 'visitors_active' ) == 1 ) {
 
@@ -21,16 +22,31 @@ class Visitors_Admin {
 
             add_filter( 'um_members_directory_sort_fields',          array( $this, 'um_members_directory_sort_fields_vv' ), 10, 1 );
             add_filter( 'um_members_directory_filter_fields',        array( $this, 'um_members_directory_sort_fields_vv' ), 10, 1 );
+
+            add_filter( 'manage_users_columns',                      array( $this, 'manage_users_columns_new_vv_columns' ));
+            add_filter( 'manage_users_custom_column',                array( $this, 'manage_users_custom_column_new_vv_columns' ), 10, 3 );
+            add_filter( 'manage_users_sortable_columns',             array( $this, 'register_sortable_columns_vv_custom' ), 10, 1 );
+
         }
 
         $um_directory_forms = get_posts( array( 'numberposts' => -1,
                                                 'post_type'   => 'um_directory',
                                                 'post_status' => 'publish'
-                                            ));
+                                            )
+                                        );
 
         foreach( $um_directory_forms as $um_form ) {
             $this->directory_forms[$um_form->ID] = $um_form->post_title;
         }
+
+        $this->vv_columns = array(
+            'vv_last_activity'    => __( 'Last activity',      'um-visitors' ),
+            'vv_last_logout'      => __( 'Last logout',        'um-visitors' ),
+            'vv_last_update'      => __( 'Last update',        'um-visitors' ),
+            'vv_visits_counter'   => __( 'Number of visits',   'um-visitors' ),
+            'vv_visitors_counter' => __( 'Number of visitors', 'um-visitors' ),
+            '_um_last_login'      => __( 'Last login',         'um-visitors' ),
+        );
     }
 
     public function load_toplevel_page_visits_visitors() {
@@ -46,18 +62,18 @@ class Visitors_Admin {
 
     public function toplevel_page_visits_visitors() {
 
-        $this->show_dashboard_metabox_content( __( 'Visitors - Profile Viewers', 'um-visitors' ),       'vv_visitors_counter', true );
-        $this->show_dashboard_metabox_content( __( 'Visits to Profile pages', 'um-visitors' ),          'vv_visits_counter',   true );
+        $this->show_dashboard_metabox_content( __( 'Profile Page Viewers',    'um-visitors' ), 'vv_visitors_counter', true );
+        $this->show_dashboard_metabox_content( __( 'Visits to Profile Pages', 'um-visitors' ), 'vv_visits_counter',   true );
         ?>
         <div style="font-weight: bold;"><?php echo __( 'Last 24 hours', 'um-visitors' ) ?> </div>
         <?php
-        $this->show_dashboard_metabox_stats( __( 'Visitors - Profile Viewers %d', 'um-visitors' ), 'vv_visitors', 1, false );
-        $this->show_dashboard_metabox_stats( __( 'Visits to Profile pages %d', 'um-visitors' ),    'vv_visits',   1, true );
+        $this->show_dashboard_metabox_stats( __( 'Profile Page Viewers %d',    'um-visitors' ), 'vv_visitors', 1, false );
+        $this->show_dashboard_metabox_stats( __( 'Visits to Profile Pages %d', 'um-visitors' ), 'vv_visits',   1, true );
         ?>
         <div style="font-weight: bold;"><?php echo __( 'Last week', 'um-visitors' ) ?> </div>
         <?php
-        $this->show_dashboard_metabox_stats( __( 'Visitors - Profile Viewers %d', 'um-visitors' ), 'vv_visitors', 7, false );
-        $this->show_dashboard_metabox_stats( __( 'Visits to Profile pages %d', 'um-visitors' ),    'vv_visits',   7, true );
+        $this->show_dashboard_metabox_stats( __( 'Profile Page Viewers %d',    'um-visitors' ), 'vv_visitors', 7, false );
+        $this->show_dashboard_metabox_stats( __( 'Visits to Profile Pages %d', 'um-visitors' ), 'vv_visits',   7, true );
         ?>
         <div style="font-weight: bold;"><?php echo __( 'Last 24 hours', 'um-visitors' ) ?> </div>
         <?php
@@ -124,15 +140,16 @@ class Visitors_Admin {
         global $wpdb;
 
         $vv_counter = $wpdb->get_results( "SELECT * FROM {$wpdb->usermeta} WHERE meta_key = '{$counter}'" );
-
+    
         if ( ! empty( $vv_counter ) && count( $vv_counter ) > 0 ) {
 
             $max = 0;
 
             foreach( $vv_counter as $counter ) {
 
-                if ( intval( $counter->meta_value ) > $max ) {
-                    $max = intval( $counter->meta_value );
+                $array = maybe_unserialize( $counter->meta_value );
+                if ( is_array( $array ) && array_sum( $array ) > $max ) {
+                    $max = array_sum( $array );
                     $user_id = $counter->user_id;
                 }
             }
@@ -159,14 +176,49 @@ class Visitors_Admin {
         return $sort_fields;
     }
 
-    public function vv_activity_shortcode() {
+    public function manage_users_columns_new_vv_columns( $columns ) {
 
-        global $current_user;
+        $this->new_vv_columns = array_map( 'sanitize_text_field', UM()->options()->get( 'vv_all_user_columns' ));
 
-        if ( UM()->options()->get( 'visitors_active' ) == 1 ) {
-
-            update_user_meta( $current_user->ID, 'vv_last_activity', date_i18n( $this->date_format, $this->current_time ) );
+        foreach( $this->new_vv_columns as $meta_key ) {
+            $columns['um_column_' . $meta_key] = esc_attr( $this->vv_columns[$meta_key] );
         }
+
+        return $columns;
+    }
+
+    public function manage_users_custom_column_new_vv_columns( $value, $column_name, $user_id ) {
+
+        foreach( $this->new_vv_columns as $meta_key ) {
+            if ( $column_name == 'um_column_' . $meta_key ) {
+
+                um_fetch_user( $user_id );
+                $value = um_user( $meta_key );
+
+                if( empty( $value )) {
+                    $value = '-';
+                }
+
+                if ( is_array( $value )) {
+                    $value = array_sum( $value );
+                }
+                break;
+            }
+        }
+
+        return $value;
+    }
+
+    public function register_sortable_columns_vv_custom( $columns ) {
+
+        foreach( $this->new_vv_columns as $meta_key ) {
+
+            if ( ! array_key_exists( 'um_column_' . $meta_key, $columns )) {
+                $columns['um_column_' . $meta_key] = 'um_column_' . $meta_key;
+            }
+        }
+
+        return $columns;
     }
 
     public function visitors_settings( $settings ) {
@@ -226,7 +278,7 @@ class Visitors_Admin {
                                     'options'     => $this->directory_forms,
                                     'label'       => __( 'Visitors - Member Directory Form', 'um-visitors' ),
                                     'description' => __( 'Create a Member Directory Form for display of User Profile Visitors.', 'um-visitors' ),
-                                    'size'        => 'medium',
+                                    'size'        => 'small',
                                     'conditional' => array( "visitors_active", '=', 1 )
                                 );
  
@@ -239,7 +291,7 @@ class Visitors_Admin {
                                     'options'     => $this->directory_forms,
                                     'label'       => __( 'Visits - Member Directory Form', 'um-visitors' ),
                                     'description' => __( 'Create a Member Directory Form for display of User Profile Visits.', 'um-visitors' ),
-                                    'size'        => 'medium',
+                                    'size'        => 'small',
                                     'conditional' => array( "visitors_active", '=', 1 )
                                 );
 
@@ -261,6 +313,16 @@ class Visitors_Admin {
                                     'type'        => 'text',
                                     'label'       => __( 'Number of counters', 'um-visitors' ),
                                     'description' => __( 'Enter the number of days/weeks for User counter displays.', 'um-visitors' ),
+                                    'size'        => 'small',
+                                    'conditional' => array( "visitors_active", '=', 1 )
+                                );
+
+            $all_fields[] = array(  'id'          => 'vv_all_user_columns',
+                                    'type'        => 'select',
+                                    'multi'       => true,
+                                    'label'       => __( 'WP All Users columns', 'um-visitors' ),
+                                    'description' => __( 'Select the sortable columns for Plugin\'s UM Predefined fields in WP All Users page.', 'um-visitors' ),
+                                    'options'     => $this->vv_columns,
                                     'size'        => 'small',
                                     'conditional' => array( "visitors_active", '=', 1 )
                                 );
@@ -354,28 +416,6 @@ class Visitors_Admin {
                                             'pretty_format' => 1,
                                             'years'         => 115,
                                             'years_x'       => 'past',
-                                        );
-
-        $predefined_fields['vv_visits_counter'] = array(
-                                            'title'           => __( 'Number of visits','um-visitors' ),
-                                            'metakey'         => 'vv_visits_counter',
-                                            'type'            => 'text',
-                                            'label'           => __( 'Visits','um-visitors' ),
-                                            'required'        => 0,
-                                            'public'          => 1,
-                                            'editable'        => false,
-                                            'edit_forbidden'  => 1,
-                                        );
-
-        $predefined_fields['vv_visitors_counter'] = array(
-                                            'title'           => __( 'Number of visitors','um-visitors' ),
-                                            'metakey'         => 'vv_visitors_counter',
-                                            'type'            => 'text',
-                                            'label'           => __( 'Visitors','um-visitors' ),
-                                            'required'        => 0,
-                                            'public'          => 1,
-                                            'editable'        => false,
-                                            'edit_forbidden'  => 1,
                                         );
 
         return $predefined_fields;
